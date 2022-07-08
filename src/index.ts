4e4c0d6aba7fe680279306d193ecfd141e443415
Jim Slusser <jslusser@kurkuma.co>
2022-07-08 17:39:43 -0400

    Initial commit

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,6 @@
+const PROGRAM_ADDRESS = 'ChT1B39WKLS8qUrkLvFDXMhEJ4F1XZzwUNHUt4AU9aVa'
+const PROGRAM_DATA_ADDRESS = 'Ah9K7dQ8EHaZqcAsgBW8w37yN2eAy3koFmUn4x3CJtod'
+
 import * as Web3 from '@solana/web3.js'
 import Dotenv from 'Dotenv'
 Dotenv.config()
@@ -5,9 +8,37 @@ Dotenv.config()
 async function main() {
     const payer = initializeKeypair()
     const connection = new Web3.Connection(Web3.clusterApiUrl('devnet'))
+    await pingProgram(connection, payer)
     await connection.requestAirdrop(payer.publicKey, Web3.LAMPORTS_PER_SOL*1)
     await sendSol(connection, 0.1*Web3.LAMPORTS_PER_SOL, Web3.Keypair.generate().publicKey, payer)
 }
+
+async function pingProgram(connection: Web3.Connection, payer: Web3.Keypair) {
+    const transaction = new Web3.Transaction()
+
+    const programId = new Web3.PublicKey(PROGRAM_ADDRESS)
+    const programDataPubkey = new Web3.PublicKey(PROGRAM_DATA_ADDRESS)
+
+    const instruction = new Web3.TransactionInstruction({
+        keys: [
+            {
+                pubkey: programDataPubkey,
+                isSigner: false,
+                isWritable: true
+            },
+        ],
+        programId
+    })
+    transaction.add(instruction)
+
+    const sig = await Web3.sendAndConfirmTransaction(
+        connection,
+        transaction,
+        [payer]
+    )
+
+    console.log(sig)
+ }
 
 function initializeKeypair(): Web3.Keypair {
     const secret = JSON.parse(process.env.PRIVATE_KEY ?? "") as number[]
